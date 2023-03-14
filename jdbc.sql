8399ea336d983cccf809c40b535c0229c0786711
bhavana madas <80811054+bhavana791@users.noreply.github.com>
2023-03-14 23:22:14 +0530

    Add files via upload

---
+++ jdbc.sql
@@ -0,0 +1,16 @@
+use jdbc6;
+show tables;
+drop table stud;
+drop table registration;
+show tables;
+table stud_data;
+show tables;
+table stud_data;
+UPDATE   stud_data
+SET dob = '2000-11-20', state= 'gujrat'
+WHERE Stud_id = 101;
+DELETE FROM stud_data WHERE first_name='bhavana';
+table stud_data;
+
+
+
